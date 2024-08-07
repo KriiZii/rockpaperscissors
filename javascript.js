97937400d934d3c23e52c8bc94d6847d6c54a77b
KriiZii <39064251+KriiZii@users.noreply.github.com>
2024-08-07 05:04:46 +0200

    changed else ifs in logic

--- javascript.js
+++ javascript.js
@@ -26,43 +26,51 @@ const results = document.querySelector(".results");
 function playRound(humanChoice, computerChoice) {
     human.textContent = `Your choice is: ${humanChoice}`;
     computer.textContent = `The computers choice is: ${computerChoice}`;
+
     if (humanChoice === computerChoice) {
         results.textContent = "Draw! No one gets points";
         score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
     }
-    else if (humanChoice === "rock" && computerChoice === "paper") {
-        results.textContent = "You lose! Paper beats Rock!";
-        computerScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+    else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            results.textContent = "You lose! Paper beats Rock!";
+            computerScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
+        else if (computerChoice === "scissors") {
+            results.textContent = "You win! Rock beats Scissors!";
+            humanScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
     }
-    else if (humanChoice === "rock" && computerChoice === "scissors") {
-        results.textContent = "You win! Rock beats Scissors!";
-        humanScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+    else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            results.textContent = "You lose! Scissors beat Paper!";
+            computerScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
+        else if (computerChoice === "rock") {
+            results.textContent = "You win! Paper beats Rock!";
+            humanScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
     }
-    else if (humanChoice === "paper" && computerChoice === "scissors") {
-        results.textContent = "You lose! Scissors beat Paper!";
-        computerScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock") {
-        results.textContent = "You win! Paper beats Rock!";
-        humanScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "rock") {
-        results.textContent = "You lose! Rock beats Scissors!";
-        computerScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "paper") {
-        results.textContent = "You win! Scissors beat Paper!";
-        humanScore ++;
-        score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            results.textContent = "You lose! Rock beats Scissors!";
+            computerScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
+        else if (computerChoice === "paper") {
+            results.textContent = "You win! Scissors beat Paper!";
+            humanScore ++;
+            score.textContent = `You: ${humanScore}, Computer: ${computerScore}`;
+        }
     }
     else {
         results.textContent = "Something went wrong!";
     }
+    
     checkScore();
 }
 
